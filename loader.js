8cc44aa0f7b7ae8f8858eef59f863fc153f2409a
Hunar Khanna <teammates.hunar@gmail.com>
2018-02-15 23:25:48 +0800

    fetch and log tracking data from bpost

---
+++ loader.js
@@ -0,0 +1,4 @@
+var node = document.getElementById('main');
+var app = Elm.Main.embed(node);
+// Note: if your Elm module is named "MyThing.Root" you
+// would call "Elm.MyThing.Root.embed(node)" instead.
--- test.elm
+++ test.elm
@@ -1,32 +1,68 @@
 import Html exposing (beginnerProgram, div, button, text, Html)
 import Html.Events exposing (onClick)
+import Debug
+import Http
 
 main =
-  beginnerProgram { model = model, view = view, update = update }
+  Html.program { subscriptions = subscriptions
+               , view = view
+               , update = update
+               , init = init }
 
-type alias  Model = Int
+type alias Model =
+    { counter : Int
+    , responseData : String
+    }
 
+init : (Model, Cmd Msg)
+init = ( { counter = 0, responseData = "" }
+       , getXmlResponse
+       )
 
-model : Model
-model = 0
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
-view : model -> Html Msg
+getXmlResponse : Cmd Msg
+getXmlResponse =
+    let
+        trackingUrl = getBpostURL "312014853400000769000219190151"
+    in
+        Http.send XmlResponse (Http.getString trackingUrl)
+
+view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
+    , div [] [ text (toString model.counter) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
 
 
-type Msg = Increment | Decrement
+type Msg = Increment
+         | Decrement
+         | XmlResponse (Result Http.Error String)
 
+ -- TODO organize into different folders
 
-update : Msg -> Model -> Model
+getBpostURL : String -> String
+getBpostURL trackCode =
+    "http://www.bpost2.be/bpostinternational/track_trace/find.php?search=s&lng=en&trackcode=" ++ trackCode
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      ({ model | counter = model.counter + 1}, Cmd.none)
 
     Decrement ->
-      model - 1
+      ({ model | counter = model.counter - 1 }, Cmd.none)
+
+    XmlResponse (Ok data) ->
+        let
+            loggedData = Debug.log "Data" data
+        in
+            ({ model | responseData = loggedData }, Cmd.none)
+
+    XmlResponse (Err _) ->
+            ({ model | responseData = "Error encountered"}, Cmd.none)
