41c2f66a8537a5f7bb78457e10d4f9c074e37374
Hunar Khanna <teammates.hunar@gmail.com>
2018-02-18 22:43:14 +0800

    add decoders for deeply nested xml nodes with tests

--- src/Models/Types.elm
+++ src/Models/Types.elm
@@ -1,7 +1,68 @@
 module Models.Types exposing (..)
 
-type alias BpostData =
-    { responseData : String }
+import Xml.Decode as XD
+import Xml.Decode.Extra exposing (..)
 
-someString : String
-someString = "Some string"
+type StatusMessage = StatusMessage String
+
+type Status =
+    Status { dateTime : String
+           , statusMessage : StatusMessage
+           , location : Maybe String
+           }
+
+
+-- type Status = Status String
+
+-- type DeliveryStatus =
+--     DeliveryStatus
+--     { statusList : List Status }
+
+-- type Parcel =
+--     Parcel
+--     { trackingBarcode : String
+--     , deliveryStatus : DeliveryStatus
+--     }
+
+-- type Parcels =
+--     Parcels
+--     { parcel : Parcel
+--     }
+
+
+statusMessageDecoder : XD.Decoder StatusMessage
+statusMessageDecoder =
+    let
+        strExtractor : XD.Decoder String
+        strExtractor = XD.path ["en"] (XD.single XD.string)
+    in
+        XD.map (\s -> StatusMessage s) strExtractor
+
+statusDecoder : XD.Decoder Status
+statusDecoder =
+    let
+        ---
+        locationDecoder : XD.Decoder (Maybe String)
+        locationDecoder =
+            XD.path ["location"] (XD.single XD.string)
+                |> XD.maybe
+
+        dateTimeDecoder : XD.Decoder String
+        dateTimeDecoder =
+            XD.path ["dateTime"] (XD.single XD.string)
+
+        statusDecoder : XD.Decoder StatusMessage
+        statusDecoder =
+            XD.path ["status"] (XD.single statusMessageDecoder)
+
+        lambda : String -> StatusMessage -> (Maybe String) -> Status
+        lambda dateTime status maybeLocation =
+            Status { dateTime = dateTime
+                   , statusMessage = status
+                   , location = maybeLocation
+                   }
+    in
+        XD.succeed (lambda)
+            |: dateTimeDecoder
+            |: statusDecoder
+            |: locationDecoder
--- tests/DecoderTests.elm
+++ tests/DecoderTests.elm
@@ -1,16 +1,51 @@
 module DecoderTests exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
+import Models.Types as Bpost exposing (..)
 
+import Xml.Decode as XD exposing ( path
+                           , single
+                           , string )
 
 suite : Test
 suite =
     describe "Model.Types"
-        [ describe "Low-level parser"
-             [ test "can decode xml into the type" <|
+        [ describe "StatusDecoder"
+             [ test "can decode xml value into correct type" <|
                   \_ ->
-                      Expect.equal 1 1
+                   let
+                       xmlString =
+                           """
+                           <status>
+                            <location>Belgium</location>
+                            <dateTime>someDateTime</dateTime>
+                            <status>
+                            <en>Parcel is handled</en>
+                            </status>
+                            <statusCode>BI3</statusCode>
+                           </status>
+                           """
+                   in
+                       Expect.equal
+                           (XD.run Bpost.statusDecoder xmlString)
+                           (Ok (Status { dateTime = "someDateTime"
+                                       , statusMessage = StatusMessage "Parcel is handled"
+                                       , location = Just "Belgium"
+                                       }))
+
+             , test "can correctly decode english status text" <|
+                 \_ ->
+                     let
+                         xmlString =
+                             """
+                             <status>
+                              <en>Parcel is handled</en>
+                             </status>
+                             """
+                     in
+                         Expect.equal
+                             (XD.run Bpost.statusMessageDecoder xmlString)
+                             (Ok (Bpost.StatusMessage "Parcel is handled"))
              ]
         ]
