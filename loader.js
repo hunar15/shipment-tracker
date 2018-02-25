93d90f30226ac51589512fdb362380ae25afc769
Hunar Khanna <teammates.hunar@gmail.com>
2018-02-25 19:18:30 +0800

    use local storage instead of sync storage

--- loader.js
+++ loader.js
@@ -3,12 +3,12 @@ var app = Elm.Main.embed(node);
 // Note: if your Elm module is named "MyThing.Root" you
 // would call "Elm.MyThing.Root.embed(node)" instead.
 
-chrome.storage.sync.get("trackingIds", function(items) {
+chrome.storage.local.get("trackingIds", function(items) {
     app.ports.loadTrackingIds.send(items['trackingIds']);
 });
 
 app.ports.updateStorage.subscribe(function(trackingIds) {
-    chrome.storage.sync.set({
+    chrome.storage.local.set({
         "trackingIds": trackingIds
     }, function() {
         console.log("message saved");
